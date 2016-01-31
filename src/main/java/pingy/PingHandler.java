2a4e51090bcf793ed6569c8edd4291012690fc79
Massimo Battestini <massimob76@gmail.com>
2016-01-31 18:38:00 +0000

    adds logging when ping event received

--- src/main/java/pingy/PingHandler.java
+++ src/main/java/pingy/PingHandler.java
@@ -2,6 +2,8 @@ package pingy;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.OutputStream;
@@ -9,13 +11,15 @@ import java.io.OutputStream;
 public class PingHandler implements HttpHandler {
 
     private static final int HTTP_OK = 200;
+    private static final Logger LOGGER = LoggerFactory.getLogger(PingHandler.class);
+    private static final String RESPONSE = "Received!";
 
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
-        String response = "Received!";
-        httpExchange.sendResponseHeaders(HTTP_OK, response.length());
+        LOGGER.info("received ping!");
+        httpExchange.sendResponseHeaders(HTTP_OK, RESPONSE.length());
         OutputStream os = httpExchange.getResponseBody();
-        os.write(response.getBytes());
+        os.write(RESPONSE.getBytes());
         os.close();
     }
 }
